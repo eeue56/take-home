6710c042777a9e0848a6ebd68f7139082bd42f19
eeue56 <enalicho@gmail.com>
2016-02-05 19:56:45 +0000

    actually return greenhouse users instead of json values

--- src/Greenhouse.elm
+++ src/Greenhouse.elm
@@ -1,7 +1,7 @@
 module Greenhouse where
 
 import Task exposing (Task)
-import Json.Decode exposing (..)
+import Json.Decode as Json exposing (..)
 import Json.Decode.Extra exposing (apply, (|:))
 import Native.Greenhouse
 
@@ -25,18 +25,37 @@ userDecoder =
         |: ("role" := list int)
 
 
+{-|
+Attempt to decode a list of json values into users
+If any user fails to decode, drop it
+-}
+decodeUsers : List Json.Value -> List User
+decodeUsers users =
+    List.foldl
+        (\user acc ->
+            case Json.decodeValue userDecoder user of
+                Ok actualUser ->
+                    actualUser :: acc
+
+                Err _ ->
+                    acc
+        )
+        []
+        users
+
+
 get : String -> String -> PageIndex -> Int -> Task String (List Value, PageIndex)
 get authToken url pageNumber numberPerPage =
     Native.Greenhouse.get authToken url pageNumber numberPerPage
 
 
-getUsers : String -> PageIndex -> Int -> Task String (List Value)
+getUsers : String -> PageIndex -> Int -> Task String (List User)
 getUsers authToken pageNumber numberPerPage =
     let
         nextPageNumber =
             pageNumber + 1
 
-        recurse : (List Value, PageIndex) -> Task String (List Value)
+        recurse : (List User, PageIndex) -> Task String (List User)
         recurse (users, endNumber) =
             if endNumber == pageNumber then
                 Task.succeed users
@@ -45,5 +64,5 @@ getUsers authToken pageNumber numberPerPage =
 
     in
         get authToken "/v1/candidates" pageNumber numberPerPage
-            |> (flip Task.andThen) recurse
+            |> (flip Task.andThen) (\(users, index) -> recurse (decodeUsers users, index) )
 
